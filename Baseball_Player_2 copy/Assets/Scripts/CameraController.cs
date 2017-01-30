9a2f921f6afc7720b668eddc80bbe570e320831e
kehlin swain <kehlinswain@gmail.com>
2017-01-30 14:23:47 -0600

    unity app

---
+++ Baseball_Player_2 copy/Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+    This class is responsible for receiving swipe inputs and manage camera position based on camera_pivot game object
+**/
+public class CameraController : MonoBehaviour {
+
+    public Transform[] cameraPivots;
+    public float changeSpeed = 0.1f;
+    private int currentPivot;
+
+
+
+	// Use this for initialization
+	void Start () {
+        currentPivot = 0;
+	}
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ChangePivot(-1);
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            ChangePivot(1);
+        }
+        if(this.transform.position != cameraPivots[currentPivot].position)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, cameraPivots[currentPivot].position, changeSpeed);
+            this.transform.rotation = cameraPivots[currentPivot].rotation;
+        }
+		
+	}
+    // @param side to go: 1 for right, -1 for left (getted by user input)
+    public int ChangePivot(int changeValue) {
+        currentPivot += changeValue;
+        if (currentPivot < 0) {
+            currentPivot = 3;
+        } else if(currentPivot > 3) {
+            currentPivot = 0;
+        }
+        return currentPivot;
+    }
+}
---
+++ Baseball_Player_2 copy/Assets/Scripts/SwingManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class SwingManager : MonoBehaviour {
+
+    public Animation anim;
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        AnimationCurve curve = AnimationCurve.Linear(0.0F, 1.0F, 2.0F, 0.0F);
+        AnimationClip clip = new AnimationClip();
+        clip.legacy = true;
+        clip.SetCurve("", typeof(Transform), "localPosition.x", curve);
+        anim.AddClip(clip, "test");
+        anim.Play("test");
+    }
+}
